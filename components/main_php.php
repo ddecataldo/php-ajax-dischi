18b9b9dab16446be2de94a79cd5827e49ee72003
ddecataldo <davidedecataldo92@gmail.com>
2021-12-01 17:51:36 +0100

    Main php

---
+++ components/main_php.php
@@ -0,0 +1,15 @@
+<main>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-5 g-4 py-5">      
+                
+                <?php
+                    require_once __DIR__ . "/card_php.php";
+                ?>
+
+        </div>
+    </div>
+
+    
+
+</div>
+</main> 
